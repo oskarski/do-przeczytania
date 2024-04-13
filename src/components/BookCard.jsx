541f66b5c4078285ef2ccd11d57bb4accab37210
Oskar Kupski <oskar.kupski@gmail.com>
2024-04-13 13:47:10 +0200

    extracted `<UpdateBookForm />`

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,17 +1,9 @@
-import { useId, useState } from "react";
+import { useState } from "react";
 import { DeleteBookButton } from "./DeleteBookButton";
-import { Label } from "../ui/Label";
-import { Input } from "../ui/Input";
-import { Button } from "../ui/Button";
-import { validateBookData } from "./validateBookData";
-import { FieldErrorMessage } from "../ui/FieldErrorMessage";
+import { UpdateBookForm } from "./UpdateBookForm";
 
 export const BookCard = ({ book, onDeleteBook, onEditBook }) => {
-  const [errors, setErrors] = useState({});
   const [editMode, setEditMode] = useState(false);
-
-  const titleId = useId();
-  const authorId = useId();
 
   return (
     <li className="bg-gray-200 p-6 rounded-lg flex justify-between">
@@ -23,57 +15,18 @@ export const BookCard = ({ book, onDeleteBook, onEditBook }) => {
       )}
 
       {editMode && (
-        <div>
-          <form
-            onSubmit={(e) => {
-              e.preventDefault();
-
-              const formData = new FormData(e.target);
-
-              const title = formData.get("title");
-              const author = formData.get("author");
-
-              const updateBookData = { title, author };
-              const validationErrors = validateBookData(updateBookData);
-              const hasErrors = Object.keys(validationErrors).length > 0;
-
-              if (hasErrors) {
-                setErrors(validationErrors);
-                return;
-              }
-
-              onEditBook({ id: book.id, ...updateBookData });
-              setErrors({});
-              setEditMode(false);
-            }}
-          >
-            <Label htmlFor={titleId}>Tytył książki</Label>
-            <Input id={titleId} name="title" defaultValue={book.title} />
-            <FieldErrorMessage error={errors.title} />
-
-            <Label htmlFor={authorId}>Autor książki</Label>
-            <Input id={authorId} name="author" defaultValue={book.author} />
-            <FieldErrorMessage error={errors.author} />
-
-            <div className="mt-3">
-              <Button>Zapisz</Button>
-            </div>
-          </form>
-        </div>
+        <UpdateBookForm
+          book={book}
+          onEditBook={(updatedBook) => {
+            onEditBook(updatedBook);
+            setEditMode(false);
+          }}
+        />
       )}
 
       <div className="text-right space-x-3">
         {!editMode && <button onClick={() => setEditMode(true)}>Edytuj</button>}
-        {editMode && (
-          <button
-            onClick={() => {
-              setEditMode(false);
-              setErrors({});
-            }}
-          >
-            Anuluj
-          </button>
-        )}
+        {editMode && <button onClick={() => setEditMode(false)}>Anuluj</button>}
 
         <DeleteBookButton onDeleteBook={onDeleteBook} />
       </div>
---
+++ src/components/UpdateBookForm.jsx
@@ -0,0 +1,50 @@
+import { useId, useState } from "react";
+import { validateBookData } from "./validateBookData";
+import { Label } from "../ui/Label";
+import { Input } from "../ui/Input";
+import { FieldErrorMessage } from "../ui/FieldErrorMessage";
+import { Button } from "../ui/Button";
+
+export const UpdateBookForm = ({ book, onEditBook }) => {
+  const [errors, setErrors] = useState({});
+
+  const titleId = useId();
+  const authorId = useId();
+
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.target);
+
+        const title = formData.get("title");
+        const author = formData.get("author");
+
+        const updateBookData = { title, author };
+        const validationErrors = validateBookData(updateBookData);
+        const hasErrors = Object.keys(validationErrors).length > 0;
+
+        if (hasErrors) {
+          setErrors(validationErrors);
+          return;
+        }
+
+        onEditBook({ id: book.id, ...updateBookData });
+        setErrors({});
+      }}
+    >
+      <Label htmlFor={titleId}>Tytył książki</Label>
+      <Input id={titleId} name="title" defaultValue={book.title} />
+      <FieldErrorMessage error={errors.title} />
+
+      <Label htmlFor={authorId}>Autor książki</Label>
+      <Input id={authorId} name="author" defaultValue={book.author} />
+      <FieldErrorMessage error={errors.author} />
+
+      <div className="mt-3">
+        <Button>Zapisz</Button>
+      </div>
+    </form>
+  );
+};
