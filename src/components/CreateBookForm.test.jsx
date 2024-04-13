adef70518cd3c034a59e1bdb2bdb3177f1edd423
Oskar Kupski <oskar.kupski@gmail.com>
2024-04-13 16:06:26 +0200

    added test for form validation

--- src/components/CreateBookForm.test.jsx
+++ src/components/CreateBookForm.test.jsx
@@ -26,5 +26,24 @@ describe("<CreateBookForm />", () => {
     expect(onBookCreatedMock).toHaveBeenCalledTimes(1);
   });
 
-  it.todo("prevents adding new book when invalid data");
+  it("prevents adding new book when invalid data", () => {
+    // Given
+    const onBookCreatedMock = jest.fn();
+
+    render(<CreateBookForm onBookCreated={onBookCreatedMock} />);
+
+    // When & Then
+    act(() => userEvent.click(screen.getByText("Dodaj książkę")));
+
+    expect(screen.queryByText("Tytuł jest wymagany!")).toBeInTheDocument();
+    expect(screen.queryByText("Autor jest wymagany!")).toBeInTheDocument();
+    expect(onBookCreatedMock).not.toHaveBeenCalled();
+
+    userEvent.type(screen.getByLabelText("Tytuł książki"), "Harry Potter");
+    userEvent.type(screen.getByLabelText("Autor książki"), "J.K. Rowling");
+
+    act(() => userEvent.click(screen.getByText("Dodaj książkę")));
+
+    expect(onBookCreatedMock).toHaveBeenCalled();
+  });
 });
