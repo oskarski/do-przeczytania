f1804f8efca7d28a9ec9e5fbe74d4eb94dc932fb
Oskar Kupski <oskar.kupski@gmail.com>
2024-04-13 13:15:01 +0200

    added UI for edit form

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,14 +1,44 @@
+import { useId, useState } from "react";
 import { DeleteBookButton } from "./DeleteBookButton";
+import { Label } from "../ui/Label";
+import { Input } from "../ui/Input";
+import { Button } from "../ui/Button";
 
 export const BookCard = ({ book, onDeleteBook }) => {
+  const [editMode, setEditMode] = useState(false);
+
+  const titleId = useId();
+  const authorId = useId();
+
   return (
     <li className="bg-gray-200 p-6 rounded-lg flex justify-between">
-      <div>
-        <h3 className="text-3xl mb-1">{book.title}</h3>
-        <span className="text-md italic text-gray-700">{book.author}</span>
-      </div>
+      {!editMode && (
+        <div>
+          <h3 className="text-3xl mb-1">{book.title}</h3>
+          <span className="text-md italic text-gray-700">{book.author}</span>
+        </div>
+      )}
 
-      <div className="text-right">
+      {editMode && (
+        <div>
+          <form>
+            <Label htmlFor={titleId}>Tytył książki</Label>
+            <Input id={titleId} name="title" defaultValue={book.title} />
+
+            <Label htmlFor={authorId}>Autor książki</Label>
+            <Input id={authorId} name="author" defaultValue={book.author} />
+
+            <div className="mt-3">
+              <Button>Zapisz</Button>
+            </div>
+          </form>
+        </div>
+      )}
+
+      <div className="text-right space-x-3">
+        {!editMode && <button onClick={() => setEditMode(true)}>Edytuj</button>}
+        {editMode && <button onClick={() => setEditMode(false)}>Anuluj</button>}
+
         <DeleteBookButton onDeleteBook={onDeleteBook} />
       </div>
     </li>
