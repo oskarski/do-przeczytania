93bead377a272636772c6603fa972563389d5ddb
Oskar Kupski <oskar.kupski@gmail.com>
2024-04-13 12:03:39 +0200

    extracted `<CreateBookForm />`

--- src/App.js
+++ src/App.js
@@ -1,83 +1,38 @@
-import { useState } from "react"
-import { Label } from "./ui/Label"
-import { Input } from "./ui/Input"
-import { FieldErrorMessage } from "./ui/FieldErrorMessage";
-
-const validateCreateBookData = (data) => {
-  if (typeof data !== 'object') throw new Error('Expected object, received ' + typeof data);
-
-  const errors = {};
-
-  if (!data.title) errors['title'] = new Error('Tytuł jest wymagany!');
-  if (!data.author) errors['author'] = new Error('Autor jest wymagany!');
-
-  return errors;
-}
+import { useState } from "react";
+import { CreateBookForm } from "./components/CreateBookForm";
 
 function App() {
-  const [errors, setErrors] = useState({});
   const [books, setBooks] = useState([]);
 
   return (
     <div className="p-4 flex flex-col gap-y-8">
-      
-      <form 
-        onSubmit={e => {
-          e.preventDefault();
-          
-          const formData = new FormData(e.target);
-
-          const title = formData.get('title');
-          const author = formData.get('author');
-
-          const createBookData = { title, author };
-          const validationErrors = validateCreateBookData(createBookData);
-          const hasErrors = Object.keys(validationErrors).length > 0;
-
-          if (hasErrors) {
-            setErrors(validationErrors);
-            return
-          }
-
+      <CreateBookForm
+        onBookCreated={(newBook) => {
           // setBooks(prev => {
-          //   return [...prev, createBookData]
+          //   return [...prev, newBook]
           // });
-          setBooks(prev => [...prev, createBookData]);
-
-          setErrors({});
-          e.target.reset();
+          setBooks((prev) => [...prev, newBook]);
         }}
-        className="bg-gray-200 border-gray-500 border p-5 rounded flex gap-x-4 items-end"
-       >
-        <div className="flex-1 flex gap-x-4">
-          <div className="flex-1">
-            <Label htmlFor="title" className="block mb-2">Tytuł książki</Label>
-            {/* Label({ htmlFor: 'title', className: 'block mb-2', children: 'Tytuł ...' }) */}
-            <Input id="title" name="title" type="text" />
-            <FieldErrorMessage error={errors.title} />
-          </div>
+      />
 
-          <div className="flex-1">
-            <Label htmlFor="author" className="block mb-2">Autor książki</Label>
-            <Input id="author" name="author" type="text" />
-            <FieldErrorMessage error={errors.author} />
-          </div>
-        </div>
+      {books.length === 0 && (
+        <h2 className="text-5xl text-center">
+          Brak książek do wyświetlenia. <br /> Dodaj swoją pierwszą ksiażkę :)
+        </h2>
+      )}
 
-        <button className="bg-lime-500 text-white px-4 py-2 rounded hover:bg-lime-700 transition">Dodaj książkę</button>
-      </form>
-
-      {books.length === 0 && <h2 className="text-5xl text-center">Brak książek do wyświetlenia. <br /> Dodaj swoją pierwszą ksiażkę :)</h2>}
-
-      {books.length > 0 && <ul className="space-y-4">
-        {books.map(book =>
-          <li key={book.title} className="bg-gray-200 p-6 rounded-lg">
-            <h3 className="text-3xl mb-1">{book.title}</h3>
-            <span className="text-md italic text-gray-700">{book.author}</span>
-          </li>
-        )}
-      </ul>}
-
+      {books.length > 0 && (
+        <ul className="space-y-4">
+          {books.map((book) => (
+            <li key={book.title} className="bg-gray-200 p-6 rounded-lg">
+              <h3 className="text-3xl mb-1">{book.title}</h3>
+              <span className="text-md italic text-gray-700">
+                {book.author}
+              </span>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
---
+++ src/components/CreateBookForm.jsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+import { Input } from "../ui/Input";
+import { Label } from "../ui/Label";
+import { FieldErrorMessage } from "../ui/FieldErrorMessage";
+
+const validateCreateBookData = (data) => {
+  if (typeof data !== "object")
+    throw new Error("Expected object, received " + typeof data);
+
+  const errors = {};
+
+  if (!data.title) errors["title"] = new Error("Tytuł jest wymagany!");
+  if (!data.author) errors["author"] = new Error("Autor jest wymagany!");
+
+  return errors;
+};
+
+export const CreateBookForm = ({ onBookCreated }) => {
+  const [errors, setErrors] = useState({});
+
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.target);
+
+        const title = formData.get("title");
+        const author = formData.get("author");
+
+        const createBookData = { title, author };
+        const validationErrors = validateCreateBookData(createBookData);
+        const hasErrors = Object.keys(validationErrors).length > 0;
+
+        if (hasErrors) {
+          setErrors(validationErrors);
+          return;
+        }
+
+        onBookCreated(createBookData);
+
+        setErrors({});
+        e.target.reset();
+      }}
+      className="bg-gray-200 border-gray-500 border p-5 rounded flex gap-x-4 items-end"
+    >
+      <div className="flex-1 flex gap-x-4">
+        <div className="flex-1">
+          <Label htmlFor="title" className="block mb-2">
+            Tytuł książki
+          </Label>
+          {/* Label({ htmlFor: 'title', className: 'block mb-2', children: 'Tytuł ...' }) */}
+          <Input id="title" name="title" type="text" />
+          <FieldErrorMessage error={errors.title} />
+        </div>
+
+        <div className="flex-1">
+          <Label htmlFor="author" className="block mb-2">
+            Autor książki
+          </Label>
+          <Input id="author" name="author" type="text" />
+          <FieldErrorMessage error={errors.author} />
+        </div>
+      </div>
+
+      <button className="bg-lime-500 text-white px-4 py-2 rounded hover:bg-lime-700 transition">
+        Dodaj książkę
+      </button>
+    </form>
+  );
+};
